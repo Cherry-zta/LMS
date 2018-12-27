ea95df75e7e909160d24dd6d73304c235d3b2bcb
AlyssaOne <liyan_zheng_r@163.com>
2018-12-27 12:23:37 +0800

    class po of my part

---
+++ po/hibernate/po/Category.java
@@ -0,0 +1,22 @@
+package hibernate.po;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+	private int cid;
+	private String cname;
+	
+	public void setCid(int cid) {
+		this.cid=cid;
+	}
+	public void setCname(String cname) {
+		this.cname=cname;
+	}
+	
+	public int getCid() {
+		return this.cid;
+	}
+	public String getCname() {
+		return this.cname;
+	}
+}
---
+++ po/hibernate/po/DetailedBook.java
@@ -0,0 +1,113 @@
+package hibernate.po;
+
+import java.io.Serializable;
+
+public class DetailedBook implements Serializable {
+	private int bid;
+	private String status;
+	private String publishinghouse;
+	private String series;
+	private String isbn;
+	private int borrowtimes=0;
+	private String categorynumber;
+	private String author;
+	private String secondauthor;
+	private String introduction;
+	private String summary;
+	private String authornotes;
+	private String readernotes;
+	private String bookspecification;
+	private String doubanintroduce;
+	
+	public void setBid(int bid) {
+		this.bid=bid;
+	}
+	public void setStatus(String status) {
+		this.status=status;
+	}
+	public void setPublishinghouse(String publishinghouse) {
+		this.publishinghouse=publishinghouse;
+	}
+	public void setSeries(String series) {
+		this.series=series;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn=isbn;
+	}
+	public void setBorrowtimes(int borrowtimes) {
+		this.borrowtimes=borrowtimes;
+	}
+	public void setCategorynumber(String categorynumber) {
+		this.categorynumber=categorynumber;
+	}
+	public void setAuthor(String author) {
+		this.author=author;
+	}
+	public void setSecondauthor(String secondauthor) {
+		this.secondauthor=secondauthor;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction=introduction;
+	}
+	public void setSummary(String summary) {
+		this.summary=summary;
+	}
+	public void setAuthornotes(String authornotes) {
+		this.authornotes=authornotes;
+	}
+	public void setReadernotes(String readernotes) {
+		this.readernotes=readernotes;
+	}
+	public void setBookspecification(String bookspecification) {
+		this.bookspecification=bookspecification;
+	}
+	public void setDoubanintroduce(String doubanintroduce) {
+		this.doubanintroduce=doubanintroduce;
+	}
+	
+	public int getBid() {
+		return this.bid;
+	}
+	public String getStatus() {
+		return this.status;
+	}
+	public String getPublishinghouse() {
+		return this.publishinghouse;
+	}
+	public String getSeries() {
+		return this.series;
+	}
+	public String getIsbn() {
+		return this.isbn;
+	}
+	public int getBorrowtimes() {
+		return this.borrowtimes;
+	}
+	public String getCategorynumber() {
+		return this.categorynumber;
+	}
+	public String getAuthor() {
+		return this.author;
+	}
+	public String getSecondauthor() {
+		return this.secondauthor;
+	}
+	public String getIntroduction() {
+		return this.introduction;
+	}
+	public String getSummary() {
+		return this.summary;
+	}
+	public String getAuthornotes() {
+		return this.authornotes;
+	}
+	public String getReadernotes() {
+		return this.readernotes;
+	}
+	public String getBookspecification() {
+		return this.bookspecification;
+	}
+	public String getDoubanintroduce() {
+		return this.doubanintroduce;
+	}
+}
